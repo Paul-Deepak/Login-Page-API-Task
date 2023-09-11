b9dde7f75f4cb30b9ce0b3f49abe511625816a0d
Paul-Deepak <pauldeepak2003@gmail.com>
2023-09-11 12:23:22 +0530

    clearAll and LogOut buttons added

--- LOGIN-PAGE-formvalidation-main/LOGIN-PAGE-formvalidation-main/redirect.js
+++ LOGIN-PAGE-formvalidation-main/LOGIN-PAGE-formvalidation-main/redirect.js
@@ -9,3 +9,12 @@ function sendreq() {
     req.open("GET", "https://random-data-api.com/api/v2/users?size=100", true);
     req.send();
 }
+function sendRequest() {
+    return fetch("https://random-data-api.com/api/v2/users?size=100")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        });
+}
