08a23c35e253c578c054cb2f94008a544af1b040
Paul-Deepak <pauldeepak2003@gmail.com>
2023-09-12 11:21:52 +0530

    relevant file names

--- LOGIN-PAGE-formvalidation-main/LOGIN-PAGE-formvalidation-main/redirect.js
+++ LOGIN-PAGE-formvalidation-main/LOGIN-PAGE-formvalidation-main/redirect.js
@@ -1,15 +1,4 @@
-function sendreq() {
-    let req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let response = this.responseText;
-            localStorage.setItem('responsekey', response);
-        }
-    };
-    req.open("POST", "https://random-data-api.com/api/v2/users?size=100", true);
-    req.send();
-}
-function sendRequest() {
+async function sendRequest() {
     return fetch("https://random-data-api.com/api/v2/users?size=100")
         .then(response => {
             if (!response.ok) {
